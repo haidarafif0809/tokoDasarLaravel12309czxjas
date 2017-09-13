b0b47eba0ad2ae21988b5cffc685e8d318ae539a
Unknown <ghozam.alghifari96@gmail.com>
2017-09-13 14:14:06 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,8 @@
     <link href="{{ asset('css/bootstrap-clockpicker.min.css') }}" rel="stylesheet"> 
 
     <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'> 
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'> 
 
     <!-- Scripts -->
     <script>
@@ -43,7 +43,7 @@
             -->
 
             <div class="logo">
-                <a href="http://andaglos.id" class="simple-text">
+                <a href="https://andaglos.id" class="simple-text">
                     ANDAGLOS.ID
                 </a>
             </div>
